0e02271a6f4cfa5c18976040fcf5c932d898dfd3
Cleiton <lucascleiton180@gmail.com>
2024-03-04 17:24:59 -0300

    Resolução das questões da lista 1 e 2

--- src/poo_lista_1/lista_8_questoes.java
+++ src/poo_lista_1/lista_8_questoes.java
@@ -1,40 +1,173 @@
 package poo_lista_1;
+
 import java.util.Scanner;
+import java.util.Random;
 
 public class lista_8_questoes {
 	public static void main(String[] args) {
-		
+		calcularMedia2();
 	}
-	
-	//Questo 1: calculo da media aritimetica.
-	public static float calcularMedia(float[] array, String[] materias) {
+
+	// Questo 1: solicitação de 5 notas e realizar a média.
+	public static void calcularMedia() {
+		float[] array = new float[5];
+		String[] materias = { "Portugues", "Matematica", "Fisica", "Literatura", "Biologia" };
+
 		Scanner sc = new Scanner(System.in);
-		
-		for(int i = 0; i < array.length; i++) {
-			System.out.println("Informe suas notas de " + materias[i] + " :");
-			array[i] = sc.nextFloat();		
+
+		for (int i = 0; i < array.length; i++) {
+			System.out.println("Informe suas notas de " + materias[i] + ":");
+			array[i] = sc.nextFloat();
 		}
-		
+
 		float soma = 0;
-		
-		for(int notas = 0; notas < array.length; notas++) {
+
+		for (int notas = 0; notas < array.length; notas++) {
 			soma = soma + array[notas];
 		}
-		
+
 		float media = soma / array.length;
-		return media;
+		System.out.println("Sua media geral eh: " + media);
 	}
-	
-	//Questao 2: impressão inversa de um array com 10 elementos.
+
+	// Questao 2: impressão inversa de um array com 10 números inteiros.
 	public static void imprimirInverso(int[] array) {
-		for(int i = array.length - 1; i >= 0; i--) {
+		for (int i = array.length - 1; i >= 0; i--) {
 			System.out.print(array[i] + " ");
 		}
 	}
-	
-	//Questão 3: impressão inversa de um array com 100 elementos aleatorios.
-	public static void imprimirAleatorio(int[] array) {
-		
+
+	// Questão 3: impressão inversa de um array com 100 números inteiros.
+	public static void imprimirAleatorio() {
+		int[] array = new int[100];
+		Random gerador = new Random();
+
+		for (int i = 0; i < array.length; i++) {
+			array[i] = gerador.nextInt(101);
+			System.out.print(array[i] + " ");
+		}
+
+		System.out.println();
+
+		for (int j = array.length - 1; j >= 0; j--) {
+			System.out.print(array[j] + " ");
+		}
 	}
 
+	// Questão 4: preencher um array com 100 números inteiros aleatórios 
+	//e imprimir os números primos.
+	public static void imprimirPrimos() {
+		int[] array = new int[100];
+		Random gerador = new Random();
+
+		for (int i = 0; i < array.length; i++) {
+			array[i] = gerador.nextInt(101);
+			System.out.print(array[i] + " ");
+		}
+		
+		System.out.println();
+		
+		for(int i = 0; i < array.length; i++) {
+			int cont = 0;
+			for(int j = 1; j <= array[i]; j++) {
+					if(array[i] % j == 0) {
+						cont ++;
+					}
+			}
+			if(cont == 2) {
+				System.out.print(array[i] + " ");
+			}
+		}
+	}
+	
+	// Questão 5: preencher um array com 100 número inteiros aleatórios
+	// e imprimir apenas os ímpares.
+	public static void imprimirImpares() {
+		int[] array = new int[100];
+		Random gerador = new Random();
+
+		for (int i = 0; i < array.length; i++) {
+			array[i] = gerador.nextInt(101);
+			System.out.print(array[i] + " ");
+		}
+		
+		System.out.println();
+		
+		for(int j = 0; j < array.length; j++) {
+			if(array[j] % 2 != 0 ) {
+				System.out.print(array[j] + " ");
+			}
+		}
+	}
+	
+	// Questão 6: preencher um array com 100 números inteiros aleatórios
+	// e imprimir apenas os pares.
+	public static void imprimirPares() {
+		int[] array = new int[100];
+		Random gerador = new Random();
+
+		for (int i = 0; i < array.length; i++) {
+			array[i] = gerador.nextInt(101);
+			System.out.print(array[i] + " ");
+		}
+		
+		System.out.println();
+		
+		for(int j = 0; j < array.length; j++) {
+			if(array[j] % 2 == 0 ) {
+				System.out.print(array[j] + " ");
+			}
+		}
+	}
+	
+	// Questão 7: preencher um array com 100 números inteiros aleatórios
+	// e imprimir o maior e o menor valor;
+	public static void imprimirMaiorMenor() {
+		int[] array = new int[100];
+		int numMaior = Integer.MIN_VALUE;
+	    int numMenor = Integer.MAX_VALUE;
+		
+		Random gerador = new Random();
+
+		for (int i = 0; i < array.length; i++) {
+			array[i] = gerador.nextInt(101);
+			System.out.print(array[i] + " ");
+		}
+		
+		System.out.println();
+		
+		for(int j = 0; j < array.length; j++) {
+			if(array[j] < numMenor) {
+				numMenor = array[j];
+			}else if(array[j] > numMaior) {
+				numMaior = array[j];
+			}
+		}
+		
+		System.out.print(numMaior + " " + numMenor );
+	}
+	
+	// Questão 8: preencher um array com 100 números inteiros 
+	// e calcular a média aritmética
+	public static void calcularMedia2() {
+		int[] array = new int[10];
+		double soma = 0;
+		
+		Random gerador = new Random();
+
+		for (int i = 0; i < array.length; i++) {
+			array[i] = gerador.nextInt(10);
+			System.out.print(array[i] + " ");
+		}
+		
+		System.out.println();
+		
+		for(int j = 0; j < array.length; j++) {
+			soma = soma + array[j];
+		}
+		
+		double media = soma / array.length;
+		System.out.print(media);
+	}
+	
 }
---
+++ src/poo_lista_2/lista_5_questoes.java
@@ -0,0 +1,80 @@
+package poo_lista_2;
+import java.util.Scanner;
+
+public class lista_5_questoes {
+	public static void main(String[] args) {
+		imprimirMaior();
+	}
+	
+	// Questão 1: usando o teclado imprimir o numero do tipo int, long e float.
+	public static void imprimirClasses() {
+		
+	}
+	
+	// Questão 2: calcular imc e exibir na tela de acordo com a fórmula.
+	public static void imprimirIMC() {
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Informe seu peso:");
+		float peso = sc.nextFloat();
+		
+		System.out.println("Informe sua altura:");
+		float altura = sc.nextFloat();
+		
+		double imc = peso / Math.pow(altura, 2);
+		String resultado = String.format("%.2f", imc);
+		
+		System.out.println("O valor do seu IMC eh: " + resultado);
+	}
+	
+	// Questão 3: imprimir a área do trapézio com as medidas em cm. 
+	public static void imprimirAreaTrapezio() {
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Digite o comprimento da base maior do trapézio (em cm):");
+        double baseMaior = sc.nextDouble();
+
+        System.out.println("Digite o comprimento da base menor do trapézio (em cm):");
+        double baseMenor = sc.nextDouble();
+
+        System.out.println("Digite a altura do trapézio (em cm):");
+        double altura = sc.nextDouble();
+
+        double area = ((baseMaior + baseMenor) * altura) / 2;
+
+        System.out.println("A área do trapézio em cm² é: " + area);
+	}
+	
+	// Questão 4: receber 2 números inteiros e imprimir o maior.
+	public static void imprimirMaior() {
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Digite um número qualquer:");
+		double numP = sc.nextDouble();
+		
+		System.out.println("Digite outro número:");
+		double numS = sc.nextDouble();
+		
+		if(numP > numS) {
+			System.out.println("Esse numero eh o maior: " + numP);
+		}else if(numP < numS) {
+			System.out.println("Esse numero eh o maior: " + numS);
+		}else {
+			System.out.println("Os nnumeros digitados sao iguais");
+		}
+	}
+	
+	// Questão 5: identificar se o número é par ou ímpar.
+	public static void ehParImpar() {
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Digite seu numero para descobrir se eh Par ou Impar:");
+		double numero = sc.nextDouble();
+		
+		if(numero % 2 == 0) {
+			System.out.println("Este numero eh Par");
+		}else {
+			System.out.println("Este numero eh Impar");
+		}
+	}
+}
